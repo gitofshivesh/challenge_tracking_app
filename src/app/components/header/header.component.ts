3cbda7cdac0ea930d1de2b41378995b93494da52
SHIVESH PANDEY <152695874+gitofshivesh@users.noreply.github.com>
2024-10-28 10:29:58 +0530

    Update header.component.ts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,38 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { filter, Subscription } from 'rxjs';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   isLoggingOut = false;
 
+  isLoginPage: boolean = false;
+  private routerSubscription?: Subscription;
+
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {}
+
+  ngOnInit(): void {
+    this.routerSubscription = this.router.events
+      .pipe(
+        filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+      )
+      .subscribe(event => {
+        this.isLoginPage = event.urlAfterRedirects === '/authenticate';
+      });
+  }
 
   logout(event: Event): void {
     event.preventDefault();
@@ -34,5 +53,11 @@ export class HeaderComponent {
         window.location.href = '/authenticate';
       }
     });
+  }
+
+  ngOnDestroy(): void {    
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
   }
 }
